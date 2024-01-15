6bf94401f6c1ae649b9928d7211a33b3253dd2d1
Leandro Alves <leandro_alvesbr@outlook.com>
2024-01-15 17:00:49 -0300

    Tratando erros com exceçao personalipersonalizada

--- Xadez-console/Program.cs
+++ Xadez-console/Program.cs
@@ -8,16 +8,22 @@ namespace Xadez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(1, 3));
-            tab.colocarPeca(new Torre(tab, Cor. Branco), new Posicao(6, 5));
-            tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(7, 7));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(6, 5));
+                tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(7, 7));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
 
-            Console.WriteLine();
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
