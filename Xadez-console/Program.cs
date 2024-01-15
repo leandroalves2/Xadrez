e336f52aaa7ce3305f45995d73edd5f79b6977ea
Leandro Alves <leandro_alvesbr@outlook.com>
2024-01-15 16:41:18 -0300

    Começando a colocar peças no tabuleiro

--- Xadez-console/Program.cs
+++ Xadez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace Xadez_console
 {
@@ -8,6 +9,11 @@ namespace Xadez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(1, 3));
+            tab.colocarPeca(new Torre(tab, Cor. Branco), new Posicao(6, 5));
+            tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(7, 7));
 
             Tela.imprimirTabuleiro(tab);
 
