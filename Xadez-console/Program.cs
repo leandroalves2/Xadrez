8b40d281c7140cb3c3ec4abdfd3e8aa65f6b0996
Leandro Alves <leandro_alvesbr@outlook.com>
2024-01-15 15:47:05 -0300

    criado a classe posicao

--- Xadez-console/Program.cs
+++ Xadez-console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Xadez_console
 {
@@ -10,6 +11,10 @@ namespace Xadez_console
     {
         static void Main(string[] args)
         {
+            Posicao P = new Posicao( 3, 4);
+
+            Console.WriteLine(P);
+
         }
     }
 }
