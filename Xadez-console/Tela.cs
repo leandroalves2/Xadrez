3880b10cf96d677bd1bb76d0c77b6cba3b279ca7
Leandro Alves <leandro_alvesbr@outlook.com>
2024-01-16 16:31:04 -0300

    Colocando cor nas peças

--- Xadez-console/Tela.cs
+++ Xadez-console/Tela.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using tabuleiro;
@@ -13,6 +15,7 @@ namespace Xadez_console
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8-i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -20,11 +23,32 @@ namespace Xadez_console
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirpeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+
         }
+
+        public static void imprimirpeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branco)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+
+
+            }
+        }
+
     }
 }
